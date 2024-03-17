c519a4a0683658b6d05307896901b0a4c5a3e716
Cyclone <ANobodySystem@outlook.com>
2024-03-17 05:46:56 +0330

    Patcher Implemented. Working on WpfGfx 4.8.9181.0

--- WpfBitmapCacheIssue/Windows/ThirdWindow.xaml.cs
+++ WpfBitmapCacheIssue/Windows/ThirdWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -14,23 +16,73 @@ using System.Windows.Shapes;
 
 namespace WpfBitmapCacheIssue
 {
-    /// <summary>
-    /// Interaction logic for FirstWindow.xaml
-    /// </summary>
     public partial class ThirdWindow : Window
     {
         public ThirdWindow()
         {
             InitializeComponent();
+            if (!FindWpfGfxBaseAddress())
+            {
+                Console.WriteLine($"Error : FindDirtyRegionAddress Failed.");
+            }
         }
 
         private void EnableDirtyRect(object sender, RoutedEventArgs e)
         {
-    
+            if (PatchDirtyRegionEnabled(true))
+                Console.WriteLine($" >> Dirty Rect Rendering Enabled.");
+            else
+                Console.WriteLine($"Error : EnableDirtyRect::PatchDirtyRegionEnabled Failed.");
+
         }
         private void DisableDirtyRect(object sender, RoutedEventArgs e)
         {
+            if (PatchDirtyRegionEnabled(false))
+                Console.WriteLine($" >> Dirty Rect Rendering Disabled.");
+            else
+                Console.WriteLine($"Error : DisableDirtyRect::PatchDirtyRegionEnabled Failed.");
+        }
 
+        // Patcher
+        internal static IntPtr WpfGfxBaseAdress = IntPtr.Zero;
+        internal const int DirtyRegion_Enabled_Offset = 0x170190; // Extracted by OffsetExtractor
+        internal bool FindWpfGfxBaseAddress()
+        {
+            try
+            {
+                Process process = Process.GetCurrentProcess();
+
+                foreach (ProcessModule module in process.Modules)
+                {
+                    if (module.ModuleName.ToLower() == "wpfgfx_v0400.dll")
+                    {
+                        WpfGfxBaseAdress = module.BaseAddress;
+                        Console.WriteLine("Base address of wpfgfx_v0400.dll: " + WpfGfxBaseAdress.ToString("X"));
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine($"FindDirtyRegionAddress Error:\n{error.Message}");
+                return false;
+            }
+        }
+        internal bool PatchDirtyRegionEnabled(bool Enabled)
+        {
+            try
+            {
+                if (WpfGfxBaseAdress == IntPtr.Zero) return false;
+                Marshal.WriteInt32(WpfGfxBaseAdress, DirtyRegion_Enabled_Offset, Enabled ? 1 : 0);
+                return true;
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine($"FindDirtyRegionAddress Error:\n{error.Message}");
+                return false;
+            }
         }
     }
 }
